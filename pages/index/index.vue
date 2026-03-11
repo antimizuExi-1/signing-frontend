c2660dc171f3e78a7ffb964637fc99d13619b6e6
AntimizuExi <antimizuExi@outlook.com>
2026-03-11 15:52:24 +0800

    添加相机调用，当扫描到二维码时弹出窗口显示二维码信息

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,52 @@
 <template>
-	<text>test</text>
+	<view class="content">
+		<button @click="openCamera()">Open Camera</button>
+		<!-- 弹窗 扫描二维码后显示二维码信息 -->
+		<uni-popup ref="popup" type="center">
+			<view style="width: 40vh; height: 80vh; background-color: white;">
+				{{scanType}}
+				<br>
+				{{scanResult}}
+			</view>
+		</uni-popup>
+	</view>
 </template>
 
-<script>
+<script setup>
+	import {
+		ref,
+		onMounted
+	} from 'vue'
+
+	const popup = ref(null)
+	const scanType = ref('')
+	const scanResult = ref('')
+
+	// 调用相机 
+	const openCamera = () => {
+		scanType.value = ''
+		scanResult.value = ''
+		uni.scanCode({
+			scanType: ['qrCode'],
+			success: function(res) {
+				scanType.value = res.scanType
+				scanResult.value = res.result
+				popup.value.open()
+			}
+		});
+	}
+
+	onMounted(() => {
+		console.log('init...')
+	})
 </script>
 
 <style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 90vh;
+	}
 </style>
